5004c5014b5e8049b11f7a243bc9a9b26c2e1630
Ratanaksambat19 <doung.ratanaksambat19@kit.edu.kh>
2020-07-31 01:00:33 +0700

    updateed

---
+++ step.py
@@ -0,0 +1,64 @@
+# def step(n):
+#     string = ''
+#     for _ in range(n):
+#         string += '#'
+#         print(string)
+# print(step(5))
+
+# def step(n):
+#     string = ''
+#     i = 0
+#     while i < n:
+#         string += '#'
+#         i += 1
+#         print(string)
+# print(step(5))
+
+# def step(n):
+#     i = 1
+#     while i <= n:
+#         print("#" * i)
+#         i += 1
+# print(step(5))
+
+# def step(n):
+
+#     for row in range(n):
+#         string = ''
+#         for col in range(n):
+#             if col <= row:
+#                 string += '#'
+#             else:
+#                 string += ' '
+#         print(string)
+
+# print(step(5))
+
+# def step(n):
+#     if n == 0:
+#         return 
+#     print(n * '#')
+#     return step(n - 1)
+
+# print(step(5))
+
+import sys
+sys.setrecursionlimit(10**6) 
+
+def step(n, row = 0, stair = ''):
+
+    if n == row:
+        return None
+    
+    if n == len(stair):
+        print(stair)
+        step(n, row + 1)
+
+    if len(stair) <= row:
+        stair += '#'
+    else:
+        stair += ''
+
+    step(n, row, stair)
+
+print(step(5))
